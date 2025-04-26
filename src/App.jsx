7e6e589c857ccd77b1b7b4502037321576c61fb3
Ivan Rodriguez <Irodriguez@DESKTOP-J1JP2L6>
2025-04-26 14:05:07 -0500

    corrigiendo ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // Quitamos BrowserRouter
 import Home from './componentes/Home';
 import Contact from './componentes/Contact';
 import About from './componentes/About';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
